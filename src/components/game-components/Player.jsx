49b7fb11362cedda52f7e3509256f86c0d4228f2
Tomer <tomerm1233@gmail.com>
2024-01-10 23:33:15 +0200

    player begin template

--- src/components/game-components/Player.jsx
+++ src/components/game-components/Player.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Hand from './Hand';
+import { Stack } from 'react-bootstrap';
 
-function Player({}) {
+function Player({ hands }) {
 
 
     return (
         <div className='player-hands'>
+            return (
+                <Stack direction="horizontal" gap={3}>
+                    <Hand listID={0} cards={hands[0]} key={0}/>
+                    <Hand listID={1} cards={hands[1]} key={1}/>
+                    <Hand listID={2} cards={hands[2]} key={2}/>
+                    <Hand listID={3} cards={hands[3]} key={3}/>
+                    <Hand listID={4} cards={hands[4]} key={4}/>
+                </Stack>
+            );
 
         </div>
     )
