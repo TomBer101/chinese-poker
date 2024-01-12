b37e7d252f99af99c878914a33620fde5ff83a55
Tomer <tomerm1233@gmail.com>
2024-01-12 13:32:16 +0200

    Card component in progress

--- src/components/game-components/Card.jsx
+++ src/components/game-components/Card.jsx
@@ -3,34 +3,37 @@ import React from 'react';
 import '../../styles/game-components/Card.css'
 import cardInfo from '../../utils/cardsInfo.json';
 
-function Card({ suit, rank, flip, laneID, children }) {
+function Card({ flip, laneID, children }) {
 
-    const isDiamond = suit === '◆';
+    const rank = "A";
+    const suit = "heart";
 
-    return (
-
-        
-            <div className={`game-card`} data-testid={`card-${rank}-${suit}`}>
-                {flip ? (
-                    <div className='game-card'>
-                        <img src='images/cards/BACK.svg' alt='card_back' />
-                    </div>
-                ) : (
-                    <div className='card-content card__rank-left' data-testid={`rank-${rank}`}>
-                        {rank}
-                        <div className='card__content card__suit-left' data-testid={`suit-${suit}`}> 
-                            {cardInfo["symbol"][suit]}
-                        </div>
-                        <div className='card__content card__suit-right' data-testid={`suit-${suit}`}>
-                            {cardInfo["symbol"][suit]}
-                        </div>
-                        <div className="card__content card__rank-right" data-testid={`rank-${rank}`}>{rank}</div>
-                    </div>
-                )}
+    if (flip) {
+        return (
+            <div className='game-card'>
+                <img src='images/cards/BACK.svg' alt='card_back' />
             </div>
+        )
+    }
+    return (
+        <div className={`game-card ${suit}`} data-testid={`card-${rank}-${suit}`}>
+            <div className='card-content card__rank-left' data-testid={`rank-${rank}`}>
+                {rank}
+            </div>
+            <div className='card__content card__suit-left' data-testid={`suit-${suit}`}>
+                {cardInfo["symbol"][suit]}
+            </div>
+            <div className='card__content card__suit-right' data-testid={`suit-${suit}`}>
+                {cardInfo["symbol"][suit]}
+            </div>
+            <div className="card__content card__rank-right" data-testid={`rank-${rank}`}>
+                {rank}
+            </div>
+        </div>
+    )
+}
 
-    );
-};
+
 
 
 
